bcebbd621e54418ec6a0b242aa066b193df09805
Tahir Murata <147841049+pastc@users.noreply.github.com>
2024-09-15 11:37:24 +0900

    Changed text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,10 @@
   <main>
     <div
       class="bg-[url(https://pbs.twimg.com/media/F8ywqpKa8AE6NoI?format=jpg&name=4096x4096)] bg-cover bg-no-repeat bg-center h-screen max-h-[calc(100vh-75px)] relative">
-      <h1 class="text-5xl font-semibold text-white absolute left-10 bottom-24" style="text-shadow: 0px 0px 10px black;">
+      <h1 class="text-5xl font-semibold text-white absolute left-10 bottom-10" style="text-shadow: 0px 0px 10px black;">
         AICJ 学園祭 <br /> 2024
       </h1>
-      <Button class="!absolute left-10 bottom-10" label="開催事項" as="a" href="#jikou"></Button>
+      <!-- <Button class="!absolute left-10 bottom-10" label="開催事項" as="a" href="#jikou" /> -->
 
       <div class="!absolute right-0 bottom-0">
         <span class="h-16 w-16 bg-primary text-white dark:text-black inline-flex items-center justify-center ">
@@ -15,8 +15,8 @@
     </div>
 
     <div class="grid gap-4 grid-cols-1 md:grid-cols-2 mt-10 w-full max-w-screen-xl mx-auto px-4">
-      <Card class="self-start justify-self-center p-4 border border-surface-200 dark:border-surface-700">
-        <template #title>Concept</template>
+      <Card class="self-start justify-self-center h-full p-4 border border-surface-200 dark:border-surface-700">
+        <template #title><span class="text-primary">コンセプト</span></template>
         <template #content>
           <p class="m-0">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
@@ -28,15 +28,11 @@
         </template>
       </Card>
 
-      <Card class="self-start justify-self-center p-4 border border-surface-200 dark:border-surface-700">
-        <template #title>F3SiDとは</template>
+      <Card class="self-start justify-self-center h-full p-4 border border-surface-200 dark:border-surface-700">
+        <template #title><span class="text-primary">F3SiD</span>とは</template>
         <template #content>
           <p class="m-0">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
-            numquam
-            deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse,
-            cupiditate
-            neque quas!
+            F3SiDは、学園祭で使用される一意のQRコードです。来場者はスマートフォンで専用のウェブサイトからこのQRコードを表示し、入退場や購入時に提示する必要があります。これにより、デジタルでデータが正確に管理され、在庫管理や来場者数の予測、人気展示や商品のランキングがスムーズに行えます。
           </p>
         </template>
       </Card>
