bc1d5aa485a9f6db98ad31bb8e207268a1f3e30a
Tahir Murata <147841049+pastc@users.noreply.github.com>
2024-09-15 20:13:32 +0900

    Effecient routing for api

--- src/stores/client.ts
+++ src/stores/client.ts
@@ -4,12 +4,25 @@ import { ref } from 'vue';
 export const useClientStore = defineStore('client', () => {
     const F3SiD = ref("")
 
-    async function setF3SiD() {
+    async function setF3SiD(): Promise<boolean> {
         const url = import.meta.env.VITE_API_URL;
-        const data = await fetch(url + "visitor", {
-            method: "POST"
-        }).then((r) => r.json());
-        F3SiD.value = data.f3sid
+        try {
+            const data = await fetch(url + "visitor", {
+                method: "GET"
+            });
+            if (data.status == 404) {
+                const data = await fetch(url + "visitor", {
+                    method: "POST"
+                });
+                F3SiD.value = (await data.json()).f3sid;
+                return true
+            } else {
+                F3SiD.value = (await data.json()).f3sid;
+                return true
+            }
+        } catch (e) {
+            return false
+        }
     }
 
     return { F3SiD, setF3SiD, }
