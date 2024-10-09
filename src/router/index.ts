44f4c589212f5c381745440201f52e1b39d1723a
Tahir Murata <147841049+pastc@users.noreply.github.com>
2024-10-09 11:53:54 +0900

    Added meta tags

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,45 +7,64 @@ const ScheduleView = () => import('../views/ScheduleView.vue')
 const PamphletView = () => import('../views/PamphletView.vue')
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/foods',
-      name: 'foods',
-      component: FoodsView
-    },
-    {
-      path: '/goods',
-      name: 'goods',
-      component: GoodsView
-    },
-    {
-      path: '/groups',
-      name: 'groups',
-      component: GroupsView
-    },
-    {
-      path: '/schedule',
-      name: 'schedule',
-      component: ScheduleView
-    },
-    {
-      path: '/pamphlet',
-      name: 'pamphlet',
-      component: PamphletView
-    },
-  ],
-  scrollBehavior(to, from, savedPosition) {
-    return {
-      top: 0,
-      behavior: "smooth"
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            meta: { title: 'AICJ学園祭 2024', description: 'AICJ中学・高等学校の2024学園祭公式サイトです。' },
+            component: HomeView
+        },
+        {
+            path: '/foods',
+            name: 'foods',
+            meta: { title: '高校食販', description: '高校食班のメニューです。' },
+            component: FoodsView
+        },
+        {
+            path: '/goods',
+            name: 'goods',
+            meta: { title: '物品販売', description: '物品販売のメニューです。' },
+            component: GoodsView
+        },
+        {
+            path: '/groups',
+            name: 'groups',
+            meta: { title: '参加グループ', description: '中学生クラス展示・ステージ企画・生徒企画の紹介文一覧です。' },
+            component: GroupsView
+        },
+        {
+            path: '/schedule',
+            name: 'schedule',
+            meta: { title: 'スケジュール', description: 'ステージ企画のスケジュールをご確認いただけます。' },
+            component: ScheduleView
+        },
+        {
+            path: '/pamphlet',
+            name: 'pamphlet',
+            meta: { title: 'パンフレット', description: 'データ版パンフレットをダウンロードいただけます。' },
+            component: PamphletView
+        },
+    ],
+    scrollBehavior(_to, _from, _savedPosition) {
+        return {
+            top: 0,
+            behavior: "smooth"
+        }
     }
-  }
+})
+
+router.beforeEach((to, _from, next) => {
+    const { title, description } = to.meta;
+    const defaultTitle = 'Page not found';
+    const defaultDescription = 'ページが見つかりませんでした。';
+
+    document.title = title as string || defaultTitle
+
+    const descriptionElement = document.querySelector('head meta[name="description"]')
+    if (descriptionElement)
+        descriptionElement.setAttribute('content', description as string || defaultDescription)
+    next()
 })
 
 export default router
